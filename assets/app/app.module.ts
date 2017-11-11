8da54959a4a4edc2082812ee4d8db287790a504f
ishanka25 <ishan.ranatunga@gmail.com>
2017-11-12 01:26:43 +0530

    added: authentication component messages component

--- assets/app/app.module.ts
+++ assets/app/app.module.ts
@@ -6,13 +6,17 @@ import {FormsModule} from "@angular/forms";
 import {MessageComponent} from "./messages/message.component";
 import {MessageListComponent} from "./messages/message-list.component";
 import {MessageInputComponent} from "./messages/message-input.component";
+import {MessagesComponent} from "./messages/messages.component";
+import {AuthenticationComponent} from "./auth/authentication.component";
 
 @NgModule({
     declarations: [
         AppComponent,
         MessageComponent,
         MessageListComponent,
-        MessageInputComponent
+        MessageInputComponent,
+        MessagesComponent,
+        AuthenticationComponent
     ],
     imports: [BrowserModule, FormsModule],
     bootstrap: [AppComponent]
