dd4aecfefb18123161f50549e561d4d4b3fe3ad1
ishanka25 <ishan.ranatunga@gmail.com>
2017-11-12 01:09:17 +0530

    added: message service ngForm and validations to input box

--- assets/app/messages/message-list.component.ts
+++ assets/app/messages/message-list.component.ts
@@ -1,5 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Message} from "./message.model";
+import {MessageService} from "./message.service";
 
 @Component({
     selector: 'app-message-list',
@@ -11,10 +12,12 @@ import {Message} from "./message.model";
                     *ngFor="let message of messages"></app-message>
         </div> `
 })
-export class  MessageListComponent{
-    messages: Message[] = [
-        new Message("A Message","Ishanka"),
-        new Message("Another Message","Ishanka"),
-        new Message("3rd Message","Ishanka")
-    ];
+export class  MessageListComponent implements OnInit{
+    messages: Message[];
+
+    constructor(private  messageService: MessageService){}
+
+    ngOnInit(){
+        this.messages = this.messageService.getMessages();
+    }
 }
