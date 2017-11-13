4ae121cf8884dd017f915cf98afec508fdfde57d
ishanka25 <ishan.ranatunga@gmail.com>
2017-11-13 17:48:05 +0530

    completed message edit and delete functions

--- assets/app/messages/message.service.ts
+++ assets/app/messages/message.service.ts
@@ -1,21 +1,26 @@
 import {Message} from "./message.model";
 import {Http, Response, Headers} from "@angular/http";
-import {Injectable} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 import 'rxjs/Rx';
 import {Observable} from "rxjs/Observable";
 
 @Injectable()
 export class MessageService{
     private messages: Message[] = [];
+    messageIsEdit = new EventEmitter<Message>();
 
     constructor(private http: Http) {}
 
     addMessage(message: Message){
-        this.messages.push(message);
         const body = JSON.stringify(message);
         const headers = new Headers({'Content-Type': 'application/json'});
         return this.http.post('http://localhost:3000/message', body, {headers: headers})
-            .map((response: Response) => response.json())
+            .map((response: Response) => {
+                const result = response.json();
+                const message = new Message(result.obj.content, 'Dummy', result.obj._id, null);
+                this.messages.push(message);
+                return message;
+            })
             .catch((error: Response) => Observable.throw(error.json()));
     }
 
@@ -25,7 +30,7 @@ export class MessageService{
                 const messages = response.json().obj;
                 let transformedMessages: Message[] = [];
                 for (let message of messages){
-                    transformedMessages.push(new Message(message.content, 'Dummy', message.id, null));
+                    transformedMessages.push(new Message(message.content, 'Dummy', message._id, null));
                 }
                 this.messages = transformedMessages;
                 return transformedMessages;
@@ -33,7 +38,22 @@ export class MessageService{
             .catch((error: Response) => Observable.throw(error.json()));
     }
 
+    editMessage(message: Message){
+        this.messageIsEdit.emit(message);
+    }
+
+    updateMessage(message: Message){
+        const body = JSON.stringify(message);
+        const headers = new Headers({'Content-Type': 'application/json'});
+        return this.http.patch('http://localhost:3000/message/' + message.messageId, body, {headers: headers})
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
+    }
+
     deleteMessage(message: Message){
         this.messages.splice(this.messages.indexOf(message),1);
+        return this.http.delete('http://localhost:3000/message/' + message.messageId)
+            .map((response: Response) => response.json())
+            .catch((error: Response) => Observable.throw(error.json()));
     }
 }
--- routes/messages.js
+++ routes/messages.js
@@ -37,4 +37,63 @@ router.post('/', function (req, res, next) {
     })
 });
 
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if(err){
+            return res.status(500).json({
+                title: 'An error occurred!',
+                error: err
+            });
+        }
+        if (!message){
+            return res.status(500).json({
+                title: 'No message found!',
+                error: {message: 'Message not found'}
+            });
+        }
+        message.content = req.body.content;
+        message.save(function (error, result) {
+            if (err){
+                return res.status(500).json({
+                    title: 'An error occurred!',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Message Updated!',
+                obj: result
+            });
+        })
+    })
+});
+
+router.delete('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function (err, message) {
+        if(err){
+            return res.status(500).json({
+                title: 'An error occurred!',
+                error: err
+            });
+        }
+        if (!message){
+            return res.status(500).json({
+                title: 'No message found!',
+                error: {message: 'Message not found'}
+            });
+        }
+        message.remove(function (error, result) {
+            if (err){
+                return res.status(500).json({
+                    title: 'An error occurred!',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Message Deleted!',
+                obj: result
+            });
+        })
+    })
+});
+
 module.exports = router;
